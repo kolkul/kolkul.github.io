fbac53fa80b337660ce3784ceba309938ce4aa44
kit-studio-official <kit.studio.official@gmail.com>
2021-02-18 22:03:25 +0200

    Update script.js

--- UvitMe/js/script.js
+++ UvitMe/js/script.js
@@ -48,7 +48,7 @@ function blockBody() {
 }
 
 $('body.no-scroll').on('touchmove', function () {
-  alert('touchmove');
+  $('body').css('background-color', 'red');
 })
 
 // header hide
@@ -63,7 +63,7 @@ $(window).scroll(function () {
   }
 
   if ($('body').hasClass('no-scroll')) {
-    alert('scroll');
+    $('body').css('background-color', 'green');
 
     $('body,html').animate({
       swipeUp: documentScrollStop - 150
